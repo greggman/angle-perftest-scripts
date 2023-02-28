ddfac18575b8f886662f91809f3609833b89fcbb
Gregg Tavares <gman@chromium.org>
2023-02-27 17:14:52 -0800

    use correct dir

--- run-perf-variations.sh
+++ run-perf-variations.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 set -v
+base_dir=$(dirname "$0")
 
 if [[ `git status --porcelain`  ]]; then
   echo "there are local changes. clean them up first. aborting"
@@ -16,7 +17,7 @@ function build_and_run {
   git cherry-pick perf-tweaks
   goma_ctl ensure_start
   autoninja -C out/Release angle_perftests
-  $T/run-perf-tests.sh $2
+  $base_dir/run-perf-tests.sh $2
 }
 
 sudo echo "need sudo"
@@ -24,10 +25,10 @@ sudo echo "need sudo"
 git checkout -b delme-perf-test-temp
 
 #build_and_run wait metal
-build_and_run main gl
-build_and_run main-2meg metal
-build_and_run buffersubdata-opt metal
-build_and_run bsd-old metal
+build_and_run test-perf-revert gl
+build_and_run test-perf-revert metal
+build_and_run test-perf-before metal
+#build_and_run bsd-old metal
 #build_and_run buffersubdata-opt-AMD-new-buffer metal
 #build_and_run buffersubdata-opt-fix metal
 #build_and_run buffersubdata-opt-serial metal
